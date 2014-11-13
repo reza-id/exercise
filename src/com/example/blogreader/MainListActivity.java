f37d26ec683fdf7fe63f2c4f5f1b5e8d9cdd4dac
Reza Agustiana <reza.agustiana@gmail.com>
2014-11-13 14:48:01 +0700

    add ListAdapter

--- src/com/example/blogreader/MainListActivity.java
+++ src/com/example/blogreader/MainListActivity.java
@@ -3,6 +3,7 @@ package com.example.blogreader;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 
 public class MainListActivity extends ListActivity {
 	
@@ -25,6 +26,9 @@ public class MainListActivity extends ListActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_list);
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mAndroidNames);
+		setListAdapter(adapter);
 	}
 
 	@Override
